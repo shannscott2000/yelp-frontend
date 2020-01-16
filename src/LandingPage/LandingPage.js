fbe7ca468e4aae93654d72c1b322f535954f692d
Shannon Burnham <shannscott2000@gmail.com>
2020-01-16 09:30:22 -0500

    adapt layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,12 +7,15 @@ import styles from './LandingPage.module.css';
 
 export function LandingPage() {
 	return (
-		<div>
-			<TopNav />
-			<img src={logo} className={styles.logo} alt='logo'/>
-			<SearchBar />
-			<SearchSuggestions />
+		<div className={styles.landing}>
+			<div className={styles['search-area']}>
+				<TopNav />
+				<img src={logo} className={styles.logo} alt='logo'/>
+				<SearchBar />
+				<SearchSuggestions />
+			</div>
 		</div>
+		
 	);
 }
 
