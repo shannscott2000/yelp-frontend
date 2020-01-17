8e0ffff9d96966851ff20308bfd897ed748837f0
Shannon Burnham <shannscott2000@gmail.com>
2020-01-17 10:33:39 -0500

    add Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -7,7 +7,7 @@ export function Navbar() {
 	return(
 		<div className={styles['nav-bar']}>
 			<img src={logo} className={styles.logo} alt='logo'/>
-			<SearchBar />
+			<SearchBar small/>
 			<button className={`button ${styles['nav-button']}`}>Sign In</button>
 			<button className={`button ${styles['nav-button']}`}>Register</button>
 		</div>
