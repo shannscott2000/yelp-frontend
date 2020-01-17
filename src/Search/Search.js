82f4e3fcd6e4fcd291131ed6105541a7515f3b11
Shannon Burnham <shannscott2000@gmail.com>
2020-01-17 12:11:44 -0500

    Added SubNav

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Navbar } from '../Navbar/Navbar'
-;
+import { Navbar } from '../Navbar/Navbar';
+import { SubNav } from '../SubNav/SubNav';
+
 export function Search() {
 	return (
-		<Navbar />
+		<div>
+			<Navbar />
+			<SubNav />	
+		</div>
+		
 	)
 }
